d795d1420b9866f223448fed8ad2911e24ee689f
dhruv-jethwani <dhruvjethwani56@gmail.com>
2025-08-06 16:54:18 +0530

    Handlers Func Added

--- web/handlers/ticket.py
+++ web/handlers/ticket.py
@@ -1,3 +1,8 @@
 from . import ticket_bp
+from flask import request, make_response, render_template, redirect, url_for
 UPLOAD_FOLDER = "static/uploads"
 ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg"}
+
+@ticket_bp.route("/ticket", methods=["GET"])
+def ticket():
+    return render_template("ticket.html")
