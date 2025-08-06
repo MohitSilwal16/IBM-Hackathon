0f511ce1977254153e548d193f642bd71e8fbe55
dhruv-jethwani <dhruvjethwani56@gmail.com>
2025-08-06 18:03:59 +0530

    Chatbot Message Handler Added

--- web/handlers/bot.py
+++ web/handlers/bot.py
@@ -10,3 +10,15 @@ def home():
     if not is_session_token_valid:
         return redirect(url_for("auth.about"))
     return render_template("home.html")
+
+@bot_bp.route("/message", methods = ['POST'])
+def message():
+    session_token = request.cookies.get("session-token")
+    is_session_token_valid = users.is_session_token_valid(session_token)
+    if not is_session_token_valid:
+        return redirect(url_for("auth.about"))
+
+    user_complain = request.get("com")
+    chatbot_res = chatbot.get_complain_solution(user_complain)
+    print(f"Chat Bot Response: {chatbot_res}")
+    return render_template("home.html")
